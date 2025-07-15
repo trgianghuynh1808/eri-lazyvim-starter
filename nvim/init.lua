6cd8a2cb447274e97f7654dde7356800398f014f
Giang Huynh <trgianghuynhdev1808@gmail.com>
2025-07-15 12:44:10 +0700

    feat: :wrench: add mason config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,29 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "tsserver",
+    "eslint",
+    "html",
+    "cssls",
+  },
+  handlers = {
+    function(server)
+      lspconfig[server].setup({
+        capabilities = lsp_capabilities,
+      })
+    end,
+    ["tsserver"] = function()
+      lspconfig.tsserver.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          completions = {
+            completeFunctionCalls = true,
+          },
+        },
+      })
+    end,
+  },
+})
